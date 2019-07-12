f1c5af83ef4163d6ef11aa791c7703aaa58c5dcf
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-07-12 10:31:02 +0100

    feat: Make tests dynamic
    Tests load examples and match a validator function based on a filename

--- test/index.js
+++ test/index.js
@@ -1,16 +1,22 @@
 const test = require('tape')
-const { validateObservation } = require('../')
+const validators = require('../')
+const fs = require('fs')
+const path = require('path')
 
-test('full observation validates', function (t) {
-  const obs = require('../examples/observation.full.json')
-  t.ok(validateObservation(obs), 'Observation passes validation')
-  t.error(validateObservation.errors, 'No validation errors')
-  t.end()
-})
+const examplesFolder = path.join(__dirname, '../examples')
+const examples = fs.readdirSync(examplesFolder)
 
-test('minimal observation validates', function (t) {
-  const obs = require('../examples/observation.minimal.json')
-  t.ok(validateObservation(obs), 'Observation passes validation')
-  t.error(validateObservation.errors, 'No validation errors')
+test('Validate examples', function (t) {
+  examples.forEach(filename => {
+    const validatorName = 'validate' + filename.split('.')[0].replace(/^[a-z]/, str => str.toUpperCase())
+    const validate = validators[validatorName]
+    if (typeof validate !== 'function') {
+      t.fail('No validation function found for example ' + filename)
+      return
+    }
+    const testObject = JSON.parse(fs.readFileSync(path.join(examplesFolder, filename)))
+    t.ok(validate(testObject), filename + ' validates')
+    t.error(validate.errors, 'No validation errors for ' + filename)
+  })
   t.end()
 })
