f698fe1ef7905dea6192590dfd7ba044342cc287
Tomás Ciccola <tciccola@digital-democracy.com>
2022-11-09 17:21:33 -0300

    chore: add eslint config and standard config and plugin

--- src/index.js
+++ src/index.js
@@ -1,37 +1,31 @@
-import { Observation } from '../types/observation.js';
+import { Observation } from '../types/observation.js'
 
-const schemaVersion = "1";
 const schemaTypesMap = {
-  observation: {magicByte: "1", schema: Observation }
-};
+  observation: { magicByte: '1', schema: Observation }
+}
 
 export const encode = (obj) => {
   // obj is validated against the protobuf and has the necessary fields
   // returns a buf ready to be sended to a core
-  // this bufs has the protobuf data prepended with 2 magic bytes 
+  // this bufs has the protobuf data prepended with 2 magic bytes
   // informing record type and schema version respectively
-  const schema = schemaTypesMap[obj.type].schema;
-  const buf = schema.encode(obj).finish();
-  const type = Buffer.from(schemaTypesMap[obj.type].magicByte);
-  const version = Buffer.from(schemaVersion);
-  const bufs = [type, version, buf];
-  return Buffer.concat(bufs);
-};
+  const schema = schemaTypesMap[obj.type].schema
+  const type = Buffer.from(schemaTypesMap[obj.type].magicByte)
+  const version = obj.schemaVersion
+  const buf = schema.encode(obj).finish()
+  return Buffer.concat([type, version, buf])
+}
 
-const findSchema = (type) => (acc,val) => {
-  if(schemaTypesMap[val].magicByte === type){
-    return val;
-  }
-  return acc;
-};
+const findSchema = (type) => (acc, val) => schemaTypesMap[val].magicByte === type ? val : acc
 
 export const decode = (buf) => {
   // receives a Buffer and turns it into an object validated against the schema
-  const type = buf.slice(0, 1).toString();
-  const version = buf.slice(1, 2);
-  const key = Object.keys(schemaTypesMap).reduce(findSchema(type), null)
-  const schema = schemaTypesMap[key].schema;
-  console.log("record of type", type);
-  console.log("schema version", version.toString());
-  return schema.decode(buf.slice(2, buf.length));
-};
+  const type = buf.slice(0, 1).toString()
+  const version = buf.slice(1, 2)
+  const record = buf.slice(2, buf.length)
+  const recordType = Object.keys(schemaTypesMap).reduce(findSchema(type), null)
+  const schema = schemaTypesMap[recordType].schema
+  console.log('record of type', recordType)
+  console.log('schema version', version.toString())
+  return schema.decode(record)
+}
