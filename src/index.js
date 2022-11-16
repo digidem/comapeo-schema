53b4f041fd1e68a1ac1ee41a44d80e0942092112
Tomás Ciccola <tciccola@digital-democracy.com>
2022-11-16 17:04:44 -0300

    chore: some refactor on encode fn and observation type is a literal for each type of observation

--- src/index.js
+++ src/index.js
@@ -17,18 +17,19 @@ const schemaTypesMap = {
 * @param {ObservationSchema} obj - Object to be encoded
 * @returns {Buffer} protobuf encoded buffer with 2 bytes prepended, one for the type of record and the other for the version of the schema */
 export const encode = (obj) => {
-  assert(isValid(schemaTypesMap[obj.type].jsonSchema)(obj), `invalid ${obj.type}`)
-  const schema = schemaTypesMap[obj.type].protobufSchema
-  const type = Buffer.from(schemaTypesMap[obj.type].magicByte)
+  const recordType = schemaTypesMap[obj.type]
+  assert(isValid(recordType.jsonSchema)(obj), `invalid ${obj.type}`)
+  const schema = recordType.protobufSchema
+  const type = Buffer.from(recordType.magicByte)
   const version = Buffer.from(obj.schemaVersion)
-  const buf = schema.encode(obj).finish()
-  return Buffer.concat([type, version, buf])
+  const protobuf = schema.encode(obj).finish()
+  return Buffer.concat([type, version, protobuf])
 }
 
 const findSchema = (type) => (acc, val) => schemaTypesMap[val].magicByte === type ? val : acc
 
 /** Decode a Buffer as an object validated against the corresponding schema
-* @param {Buffer} buf - protobuf encoded Buffer to be decoded (probably obtained from an hypercore)
+* @param {Buffer} buf - Buffer to be decoded (probably obtained from an hypercore)
 * @param {Object} opts - Object containing key and index of the hypercore
 * @param {Buffer} opts.key - Public key of the hypercore
 * @param {Number} opts.index - Index of the entry
@@ -40,14 +41,15 @@ export const decode = (buf, opts) => {
   assert(opts.index !== undefined && typeof opts.index === 'number', 'index should be a Number')
   const type = buf.subarray(0, 1).toString()
   const schemaVersion = buf.subarray(1, 2).toString()
-  const record = buf.subarray(2, buf.length)
   const recordType = Object.keys(schemaTypesMap).reduce(findSchema(type), null)
   const schema = schemaTypesMap[recordType].protobufSchema
-  const doc = schema.decode(record)
-  doc.id = doc.id.toString('hex')
-  doc.type = recordType
-  doc.schemaVersion = schemaVersion
-  doc.version = opts.key.toString('hex') + '/' + opts.index.toString()
-  assert(isValid(schemaTypesMap[recordType].jsonSchema)(doc), `invalid ${recordType} document!`)
-  return doc
+  const record = schema.decode(buf.subarray(2, buf.length))
+
+  record.id = record.id.toString('hex')
+  record.type = recordType
+  record.schemaVersion = schemaVersion
+  record.version = opts.key.toString('hex') + '/' + opts.index.toString()
+
+  assert(isValid(schemaTypesMap[recordType].jsonSchema)(record), `invalid ${recordType} document!`)
+  return record
 }
--- types/schema/observation.js
+++ types/schema/observation.js
@@ -1,7 +1,10 @@
 import { Type } from '@sinclair/typebox'
 
+/**
+* Observation record
+*/
 export const Observation = Type.Object({
-  type: Type.String(),
+  type: Type.Literal('observation'),
   schemaVersion: Type.String(),
   id: Type.String(),
   // createdAt: Type.Date(),
