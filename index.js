049fd23303d79a7a8dc6647a5e940ae26b74d3ac
Tomás Ciccola <tciccola@digital-democracy.com>
2022-12-14 14:08:10 -0300

    feat: validate as a separate exported fn Also: * removed assertions * index record by blockPrefix * added type checking on index.js * generate validation fns for each recordType beforehand

--- index.js
+++ index.js
@@ -1,48 +1,74 @@
+// @ts-check
 /**
  * @module mapeo-schema
  */
 
 import { Observation } from './types/proto/observation.js'
-import assert from 'node:assert'
 import fs from 'node:fs'
 import b4a from 'b4a'
 import Ajv from 'ajv'
 import addFormats from 'ajv-formats'
 
+/**
+ * @param {string} path
+ * @returns {Object}
+ */
 const loadJSON = (path) =>
-  JSON.parse(fs.readFileSync(new URL(path, import.meta.url)))
+  JSON.parse(fs.readFileSync(new URL(path, import.meta.url)).toString())
+
 const ajv = new Ajv()
 addFormats(ajv)
 
-const schemaTypesMap = {
-  observation: {
-    blockPrefix: 'obse', // 4 byte string
+const recordTypeBlockSize = 4
+const schemaVersionSize = 1
+
+// TODO: generate random 32 byte string for the value of each schema to use as a prefix
+const recordTypeToBlockPrefix = {
+  observation: 'obse',
+}
+
+const blockPrefixToSchema = {
+  obse: {
     protobufSchema: Observation,
     jsonSchema: loadJSON('./schema/observation.json'),
   },
 }
+
+// generate validating functions beforehand
+Object.keys(blockPrefixToSchema).forEach((blockPrefix) => {
+  blockPrefixToSchema[blockPrefix].validate = ajv.compile(
+    blockPrefixToSchema[blockPrefix].jsonSchema
+  )
+})
+
 /**
+ * Validate an object against the schema type
+ * @param {import('./types/schema/observation').Observation} obj - Object to be encoded
+ * @returns {Boolean} indicating if the object is valid
+ */
+export const validate = (obj) => {
+  const blockPrefix = recordTypeToBlockPrefix[obj.type]
+  return blockPrefixToSchema[blockPrefix].validate(obj)
+}
+
+/**
+ * TODO: obj should be more generic since there are other recordTypes
  * Encode a an object validated against a schema as a binary protobuf to send to an hypercore.
- * @param {import('./types/schema/observation')} obj - Object to be encoded
- * @returns {Buffer} protobuf encoded buffer with 2 bytes prepended, one for the type of record and the other for the version of the schema */
+ * @param {import('./types/schema/observation').Observation} obj - Object to be encoded
+ * @returns {Buffer} protobuf encoded buffer with recordTypeBlockSize + schemaVersionSize bytes prepended, one for the type of record and the other for the version of the schema */
 export const encode = (obj) => {
   const record = Object.assign({}, obj)
-  const recordType = schemaTypesMap[record.type]
-  const validate = ajv.compile(recordType.jsonSchema)
-  const isValid = validate(record)
-  assert(isValid, JSON.stringify(validate.errors, true, 2))
+  const blockPrefix = recordTypeToBlockPrefix[record.type]
+  const recordType = blockPrefixToSchema[blockPrefix]
   // id is a hex encoded string, but is turned into bytes when protobufed,
   // so we turn it into a buffer before that
   record.id = b4a.from(record.id, 'hex')
   const schema = recordType.protobufSchema
-  const type = b4a.from(recordType.blockPrefix)
+  const type = b4a.from(blockPrefix)
   const version = b4a.from([record.schemaVersion])
   const protobuf = schema.encode(record).finish()
   return b4a.concat([type, version, protobuf])
 }
-
-const findSchema = (type) => (acc, val) =>
-  schemaTypesMap[val].blockPrefix === type ? val : acc
 
 /**
  * Decode a Buffer as an object validated against the corresponding schema
@@ -53,32 +79,23 @@ const findSchema = (type) => (acc, val) =>
  * @returns {import('./types/schema/observation')}
  * */
 export const decode = (buf, opts) => {
-  assert(typeof opts === 'object', 'opts is missing')
-  assert(
-    opts.key !== undefined && b4a.isBuffer(opts.key),
-    'opts.key should be a Buffer'
+  const blockPrefix = buf.subarray(0, recordTypeBlockSize).toString()
+  const schemaVersion = buf.subarray(
+    recordTypeBlockSize,
+    recordTypeBlockSize + schemaVersionSize
+  )[0]
+  const schema = blockPrefixToSchema[blockPrefix].protobufSchema
+  let record = schema.decode(
+    buf.subarray(recordTypeBlockSize + schemaVersionSize, buf.length)
   )
-  assert(
-    opts.index !== undefined && typeof opts.index === 'number',
-    'index should be a Number'
-  )
-  const type = buf.subarray(0, 4).toString()
-  const schemaVersion = buf.subarray(4, 5)[0]
-  const recordType = Object.keys(schemaTypesMap).reduce(findSchema(type), null)
-  const schema = schemaTypesMap[recordType].protobufSchema
 
-  let record = schema.decode(buf.subarray(5, buf.length))
-  record = {
+  return {
     ...record,
     id: record.id.toString('hex'),
-    type: recordType,
-    schemaVersion: parseInt(schemaVersion),
+    type: Object.keys(recordTypeToBlockPrefix).find(
+      (recordType) => recordTypeToBlockPrefix[recordType] === blockPrefix
+    ),
+    schemaVersion: schemaVersion,
     version: `${opts.key.toString('hex')}/${opts.index.toString()}`,
   }
-
-  const validate = ajv.compile(schemaTypesMap[recordType].jsonSchema)
-  const isValid = validate(record)
-  assert(isValid, JSON.stringify(validate.errors, true, 2))
-
-  return record
 }
