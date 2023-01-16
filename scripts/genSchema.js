cfd38a798f16b252d335a0911e1deda294a9595a
Tomás Ciccola <tciccola@digital-democracy.com>
2023-01-16 15:30:36 -0300

    feat: add schemasPrefix export to code generation and update schemas $id to contemplate id and version

--- scripts/genSchema.js
+++ scripts/genSchema.js
@@ -12,23 +12,80 @@ import glob from 'glob-promise'
 const loadJSON = (path) =>
   JSON.parse(fs.readFileSync(new URL(path, import.meta.url)).toString())
 
-const schemas = (await glob('../schema/*.json', { cwd: 'scripts' })).map(
-  loadJSON
-)
+/**
+ * reducer; grab the jsonSchema, get the $id to grab schemaVersion and dataTypeId
+ * @param {Object} acc - accumulator
+ * @param {Object} schemas
+ * @param {Object} schemas.jsonSchema
+ * @returns {Object} obj
+ * @returns {String} obj.schemaVersion
+ * @returns {String} obj.dataTypeId
+ */
+const parseId = (acc, { jsonSchema }) => {
+  const arr = new URL(jsonSchema['$id']).pathname.split('/')
+  const schemaVersion = arr.pop()
+  const dataTypeId = arr.pop()
+  acc[jsonSchema.title] = { schemaVersion, dataTypeId }
+  return acc
+}
+/**
+ * load protobuf and jsonSchemas into object
+ * @returns {Object} schemas
+ * @returns {Object} schemas.jsonSchemas
+ * @returns {Object} schemas.protobufSchemas
+ */
+const loadSchemas = async () => {
+  return await Promise.all(
+    (
+      await glob('../schema/*.json', { cwd: 'scripts' })
+    ).map(async (path) => {
+      let protobufSchema = null
+      const jsonSchema = loadJSON(path)
+      const type = jsonSchema.title
+      try {
+        protobufSchema = (
+          await import(`../types/proto/${type.toLowerCase()}.js`)
+        )[type]
+      } catch (e) {
+        console.log('ERROR', 'protobuf schema not found', type)
+      }
+      return { jsonSchema, protobufSchema }
+    })
+  )
+}
+
+const schemas = await loadSchemas()
+// compile schemas
 const ajv = new Ajv({
-  schemas: [schemas],
+  schemas: schemas.map(({ jsonSchema }) => jsonSchema),
   code: { source: true, esm: true },
   formats: { 'date-time': true },
 })
 ajv.addKeyword('meta:enum')
 
-let mod = standaloneCode(ajv, {
-  Observation: 'http://mapeo.world/schemas/obse/4',
-  Field: 'http://mapeo.world/schemas/field.json',
-  Common: 'http://mapeo.world/schemas/common.json',
-  Filter: 'http://mapeo.world/schemas/filter.json',
-  Preset: 'http://mapeo.world/schemas/preset.json',
-})
+// generate code
+let schemaValidations = standaloneCode(
+  ajv,
+  schemas.reduce((obj, { jsonSchema }) => {
+    obj[jsonSchema['title']] = jsonSchema['$id']
+    return obj
+  }, {})
+)
+
+// serialize protobuf schemas to file?
+console.log(
+  schemas
+    .filter(({ protobufSchema }) => protobufSchema !== null)
+    .map(({ protobufSchema }) => protobufSchema.encode.toString())
+)
+
+// generate object to store schema prefixes
+const schemasPrefix = `export const schemasPrefix = ${JSON.stringify(
+  schemas.reduce(parseId, {})
+)}`
 
 const __dirname = new URL('.', import.meta.url).pathname
-fs.writeFileSync(path.join(__dirname, '../dist', 'schemas.js'), mod)
+fs.writeFileSync(
+  path.join(__dirname, '../dist', 'schemas.js'),
+  schemaValidations + schemasPrefix
+)
