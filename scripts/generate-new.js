05d2ee26a372750ec0a70f6c26c07eaa5eafc85c
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-07-20 15:10:33 +0100

    generate config file with dataTypeIds & versions

--- scripts/generate-new.js
+++ scripts/generate-new.js
@@ -4,6 +4,7 @@ import path from 'path'
 import { parseConfig } from './lib/parse-config.js'
 import { generateProtoTypes } from './lib/proto-types.js'
 import { PROJECT_ROOT } from './lib/utils.js'
+import { generateConfig } from './lib/generate-config.js'
 
 const config = parseConfig()
 const protoTypesFile = generateProtoTypes(config)
@@ -12,3 +13,7 @@ fs.writeFileSync(
   path.join(PROJECT_ROOT, 'types/proto/types.ts'),
   protoTypesFile
 )
+
+const configFile = generateConfig(config)
+
+fs.writeFileSync(path.join(PROJECT_ROOT, 'config.ts'), configFile)
---
+++ scripts/lib/generate-config.js
@@ -0,0 +1,20 @@
+/**
+ * @param {ReturnType<import('./parse-config').parseConfig>} config
+ */
+export function generateConfig({ currentSchemaVersions, dataTypeIds }) {
+  const idsLines = ['export const dataTypeIds = {']
+  for (const [schemaName, dataTypeId] of Object.entries(dataTypeIds)) {
+    idsLines.push(`  ${schemaName}: '${dataTypeId}',`)
+  }
+  idsLines.push('} as const')
+
+  const versionsLines = ['export const currentSchemaVersions = {']
+  for (const [schemaName, schemaVersion] of Object.entries(
+    currentSchemaVersions
+  )) {
+    versionsLines.push(`  ${schemaName}: ${schemaVersion},`)
+  }
+  versionsLines.push('} as const')
+
+  return [...idsLines, '', ...versionsLines, ''].join('\n')
+}
