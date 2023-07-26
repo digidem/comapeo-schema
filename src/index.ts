4aa88ff3d173510d454a8262ade21f7a9ac87ab1
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-07-26 12:10:51 +0100

    export json schema types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { encode } from './encode.js'
 export { decode } from './decode.js'
 export { currentSchemaVersions } from './config.js'
 export { validate } from './validate.js'
+
+export * from './schema/index.js'
