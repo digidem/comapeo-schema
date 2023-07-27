1636ffee31939c3f39fe3fed4f40528271f23a34
Gregor MacLennan <gmaclennan@digital-democracy.org>
2023-07-27 14:51:54 +0100

    fix: Strict types for validate function

--- src/validate.ts
+++ src/validate.ts
@@ -1,5 +1,17 @@
+import { MapeoValue, FilterBySchemaName, SchemaName } from './types.js'
 import * as validations from './validations.js'
 
-export function validate(schemaName: keyof typeof validations, obj: unknown) {
+export function validate<
+  TSchemaName extends Extract<keyof typeof validations, SchemaName>
+>(
+  schemaName: TSchemaName,
+  obj: unknown
+): obj is FilterBySchemaName<MapeoValue, TSchemaName> {
   return validations[schemaName](obj)
+}
+
+let obj = JSON.parse('')
+
+if (validate('observation', obj)) {
+  obj.attachments
 }
