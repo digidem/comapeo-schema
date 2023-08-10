be09f671501c9901fcd647320151c76b8048772b
Tomás Ciccola <tciccola@digital-democracy.com>
2023-08-10 16:45:01 -0300

    feat: add tests for extra non required values, and cleanup

--- test/index.js
+++ test/index.js
@@ -27,92 +27,43 @@ import { and } from 'ajv/dist/compile/codegen/index.js'
 }
 
 test(`testing encoding of doc with minimal required values,
-  then decoding and comparing the two objects`, async(t) => {
+  then decoding and comparing the two objects`,
+  async(t) => {
     for(let {doc,expected} of fixtures.goodDocsMinimal){
       let buf
       t.doesNotThrow(() =>  {
         buf = encode(doc)
       }, `tested encoding of ${doc.schemaName}`)
       let decodedDoc = stripUndef(decode(buf, parseVersionId(doc.versionId)))
-      t.deepEqual(decodedDoc,expected, `tested deep equal of ${doc.schemaName}`)
+      t.deepEqual(decodedDoc,expected, `tested deep equal of ${doc.schemaName} with only required values`)
     }
 })
 
 test(`testing encoding of doc with additional optional values,
-  then decoding and comparing the two objects`, async(t) => {
+  then decoding and comparing the two objects`,
+  async(t) => {
     for(let {doc,expected} of fixtures.goodDocsCompleted){
       let buf
       t.doesNotThrow(() =>  {
         buf = encode(doc)
       }, `tested encoding of ${doc.schemaName}`)
       let decodedDoc = stripUndef(decode(buf, parseVersionId(doc.versionId)))
-      t.deepEqual(decodedDoc,expected, `tested deep equal of ${doc.schemaName}`)
+      t.deepEqual(decodedDoc,expected, `tested deep equal of ${doc.schemaName} with additional values`)
     }
 })
 
-
-// test('test encoding of rightfully formated record', async (t) => {
-//   const goodDocs = Object.keys(docs.good)
-//   t.plan(goodDocs.length)
-//   goodDocs.forEach((k) => {
-//     const doc = docs.good[k]
-//     t.doesNotThrow(() => {
-//       encode(doc)
-//     }, `testing ${k}`)
-//   })
-// })
-
-// test('test validation of record', async (t) => {
-//   const goodDocs = Object.keys(docs.good)
-//   t.plan(goodDocs.length)
-//   goodDocs.forEach((k) => {
-//     const doc = docs.good[k]
-//     const record = decode(encode(doc), {
-//       coreKey: randomBytes(32),
-//       index: 0,
-//     })
-//     t.doesNotThrow(() => {
-//       // field has a type which is different from the rest :|
-//       if (k !== 'field') validate(record)
-//     }, `testing validation of ${k}`)
-//   })
-// })
-
-// test('test encoding, decoding of record and comparing the two versions', async (t) => {
-//   const goodDocs = Object.keys(docs.good)
-//   goodDocs.forEach((k) => {
-//     const doc = docs.good[k]
-//     const record = decode(encode(doc), {
-//       coreKey: randomBytes(32),
-//       index: 0,
-//     })
-//     const fields = Object.keys(doc)
-//     // t.plan(goodDocs.length * fields.length * 2)
-//     fields.forEach((f) => {
-//       console.log(typeof record[f], typeof doc[f])
-//       const msg = `checking ${f} for ${k}`
-//       record[f] && doc[f] ? t.pass(msg) : t.fail(msg)
-
-//       // if field is not an object, check equality
-//       // since objects as fields usually mean the possibility of additionalFields in jsonSchemas
-//       if (typeof record[f] !== 'object') {
-//         t.equal(record[f], doc[f], `comparing value of ${f} for ${k}`)
-//       }
-//     })
-//   })
-// })
-
-// test('test decoding of record without passing core key and index', async (t) => {
-//   const goodDocs = Object.keys(docs.good)
-//   t.plan(goodDocs.length)
-//   goodDocs.forEach((key) => {
-//     const doc = docs.good[key]
-//     const record = encode(doc)
-//     t.throws(() => {
-//       decode(record)
-//     }, `testing ${key}`)
-//   })
-// })
+test(`testing encoding of doc with additional extra values,
+then decoding and comparing the two objects - extra values shouldn't be present`,
+  async(t) => {
+    for(let {doc,expected} of fixtures.goodDocsWithExtraFields){
+      let buf
+      t.doesNotThrow(() =>  {
+        buf = encode(doc)
+      }, `tested encoding of ${doc.schemaName}`)
+      let decodedDoc = stripUndef(decode(buf, parseVersionId(doc.versionId)))
+      t.deepEqual(decodedDoc,expected, `tested deep equal of ${doc.schemaName} with extra - non valid - values`)
+    }
+})
 
 /**
  * @param {object} obj
@@ -120,12 +71,4 @@ test(`testing encoding of doc with additional optional values,
  * */
 function stripUndef(obj) {
   return JSON.parse(JSON.stringify(obj))
-}
-
-/** @param {Number} value
- * @return Number */
-function countDecimals(value) {
-  return ((value % 1) != 0)
-    ? Number(value.toString().split(".")[1].length)
-    : 0;
 }
