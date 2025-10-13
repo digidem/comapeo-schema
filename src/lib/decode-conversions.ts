78650804821fa98dcefd0c54e29da5f6ea7dfe8c
Andrew Chou <andrewchou@fastmail.com>
2025-10-13 12:56:41 -0400

    update json schema type

--- src/lib/decode-conversions.ts
+++ src/lib/decode-conversions.ts
@@ -354,6 +354,14 @@ export const convertTranslation: ConvertFunction<'translation'> = (
   }
 }
 
+function assertTrackLocationsLength<T>(
+  locations: T[]
+): asserts locations is [T, T, ...T[]] {
+  if (locations.length < 2) {
+    throw new Error('Track must have at least 2 items')
+  }
+}
+
 export const convertTrack: ConvertFunction<'track'> = (message, versionObj) => {
   const {
     common,
@@ -363,11 +371,9 @@ export const convertTrack: ConvertFunction<'track'> = (message, versionObj) => {
   } = message
   const jsonSchemaCommon = convertCommon(common, versionObj)
 
-  if (message.locations.length < 2) {
-    throw new Error('Track must have at least 2 locations')
-  }
-
   const locations = message.locations.map(convertTrackPosition)
+
+  assertTrackLocationsLength(locations)
 
   const observationRefs: Track['observationRefs'] = []
   for (const { docId, versionId } of message.observationRefs) {
